e0ebef2403a4deff09834ad4e8fc2f5a4fe1ae31
pankaj.akhade <pankaj.akhade@OPLPT089.local>
2023-09-27 19:30:34 +0530

    test jenkins

--- Jenkinsfile.api-server.groovy
+++ Jenkinsfile.api-server.groovy
@@ -1,7 +1,7 @@
 podTemplate {
     node(POD_LABEL) {
-        stage('Run shell') {
-            sh 'ls -ltr'
+        stage('SCM checkout') {
+            checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins', url: 'https://github.com/pankajakhade/awsproject.git']])
         }
     }
 }
