24a04963836313b8783edd765612bb10694bb50a
pankaj.akhade <pankaj.akhade@OPLPT089.local>
2023-09-27 20:50:20 +0530

    test jenkins

--- Jenkinsfile.api-server.groovy
+++ Jenkinsfile.api-server.groovy
@@ -1,4 +1,29 @@
-podTemplate {
+podTemplate(yaml: '''
+    apiVersion: v1
+    kind: Pod
+    spec:
+      containers:
+      - name: jnlp
+        image: jenkins/inbound-agent:3107.v665000b_51092-15
+        resources:
+          limits:
+            cpu: "500m"
+            memory: "512Mi"
+          requests:
+            cpu: "100m"
+            memory: "256Mi"
+      - name: docker
+        image: docker:dind  # Docker-in-Docker image
+        securityContext:
+          privileged: true  # Required for running Docker inside Docker
+        volumeMounts:
+          - name: docker-socket
+            mountPath: /var/run/docker.sock
+      volumes:
+        - name: docker-socket
+          hostPath:
+            path: /var/run/docker.sock
+''')  {
     node(POD_LABEL) {
         stage('SCM checkout') {
             checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins', url: 'https://github.com/pankajakhade/awsproject.git']])
