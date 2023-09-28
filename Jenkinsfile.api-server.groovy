6640d4ccbed3b26fb523982698e2c4f9756069d1
pankaj.akhade <pankaj.akhade@OPLPT089.local>
2023-09-28 12:50:44 +0530

    test jenkins

--- Jenkinsfile.api-server.groovy
+++ Jenkinsfile.api-server.groovy
@@ -7,6 +7,33 @@ podTemplate(yaml: '''
         image: docker:20.10-dind  # Docker-in-Docker image
         securityContext:
           privileged: true  # Required for running Docker inside Docker
+        volumeMounts:
+          - name: docker-data
+            mountPath: /var/lib/docker
+      - image: "jenkins/inbound-agent:3142.vcfca_0cd92128-1"
+        name: "jnlp"
+        resources:
+          requests:
+            memory: "256Mi"
+            cpu: "100m"
+        volumeMounts:
+        - mountPath: "/home/jenkins/agent"
+          name: "workspace-volume"
+          readOnly: false
+      - env:
+        - name: "JENKINS_URL"
+          value: "http://jenkins.jenkins.svc.cluster.local:8080/"
+        - name: "DOCKER_HOST"
+          value: "tcp://localhost:2375"
+      volumes:
+      - name: docker-data
+        hostPath:
+          path: /var/lib/docker
+          type: Directory
+      - name: "workspace-volume"
+        emptyDir:
+          medium: ""
+      restartPolicy: "Never"
 ''')  {
     node(POD_LABEL) {
         stage('SCM checkout') {
