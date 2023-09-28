b4632d68e4b04c533e9247d0ee0909b7fd5264be
pankaj.akhade <pankaj.akhade@OPLPT089.local>
2023-09-28 10:10:51 +0530

    test jenkins

--- Jenkinsfile.api-server.groovy
+++ Jenkinsfile.api-server.groovy
@@ -13,11 +13,12 @@ podTemplate(yaml: '''
             checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins', url: 'https://github.com/pankajakhade/awsproject.git']])
         }
         stage('Build and Push Docker Image') {
-            docker.withRegistry('043196765225.dkr.ecr.us-east-1.amazonaws.com') {
-                def customImage = docker.build(jenkins-test, "-f docker/dockerFile/Dockerfile .")
-                customImage.tag("jenkins-test:test")
-                customImage.push()
-            }
+//             docker.withRegistry('043196765225.dkr.ecr.us-east-1.amazonaws.com') {
+//                 def customImage = docker.build(jenkins-test, "-f docker/dockerFile/Dockerfile .")
+//                 customImage.tag("jenkins-test:test")
+//                 customImage.push()
+//             }
+            sh "docker images"
         }
     }
 }
