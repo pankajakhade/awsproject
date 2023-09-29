96c33598ae9326795e68445b9e0a5510d599dd37
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-29 19:29:56 +0530

    test jenkins

--- vars/scmCheckout.groovy
+++ vars/scmCheckout.groovy
@@ -1,3 +1,6 @@
-def scmCheckoutToBranch(scmUrl, credentialsId, branchName) {
-    checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
+def scmCheckoutToBranch(Map params) {
+    def scmUrl = params.scmUrl ?: error("scmUrl is required.")
+    def branchName = params.branchName ?: error("branchName is required.")
+    def credentialsId = params.credentialsId ?: error("credentialsId is required.")
+    checkout scmGit(branches: [[name: '*/' + branchName]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
 }
