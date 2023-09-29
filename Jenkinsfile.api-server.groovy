d97f5708b4835389d8a7e2e1efd74c51dff9eb2f
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-29 20:38:37 +0530

    test jenkins

--- Jenkinsfile.api-server.groovy
+++ Jenkinsfile.api-server.groovy
@@ -37,18 +37,25 @@ podTemplate(yaml: '''
     //import org.tenant.jenkinsCI //For future use
     //def object = new jenkinsCI() //For future use
     node(POD_LABEL) {
+        def imageTag = env.BRANCH_NAME + "." + sh(returnStdout: true, script: 'git rev-parse --short HEAD').toString().trim()
+        def ecrRepoURL = "https://043196765225.dkr.ecr.us-east-1.amazonaws.com"
+        def repoName = "jenkins-test"
+        def region = "us-east-1"
+        def ecrCredsInJenkins = "jenkins-ecr-creds"
+        def dockerFilePath = "docker/dockerFile/Dockerfile"
+        def dockerBuildContent = "."
+
+        def scmUrl = "https://github.com/pankajakhade/awsproject.git"
+        def branchName = "master"
+        def gitCredentialsId = "jenkins-ssh"
         try {
             stage('SCM checkout') {
-                scmCheckout.scmCheckoutAtBranch(scmUrl: "https://github.com/pankajakhade/awsproject.git", branchName: "master", credentialsId: "jenkins-ssh")
+                scmCheckout.scmCheckoutAtBranch(scmUrl: scmUrl, branchName: branchName, credentialsId: gitCredentialsId)
             }
             stage('Build and Push Docker Image') {
-                def imageTag = env.BRANCH_NAME + "." + sh(returnStdout: true, script: 'git rev-parse --short HEAD').toString().trim()
-                def ecrRepoURL = "https://043196765225.dkr.ecr.us-east-1.amazonaws.com"
-                def region = "us-east-1"
-                def ecrCredsInJenkins = "jenkins-ecr-creds"
-                def customImage = dockerOperations.dockerBuild(repoName: "jenkins-test", imageTag: imageTag,
-                dockerFilePath: "docker/dockerFile/Dockerfile",
-                dockerBuildContent: ".")
+                def customImage = dockerOperations.dockerBuild(repoName: repoName, imageTag: imageTag,
+                dockerFilePath: dockerFilePath,
+                dockerBuildContent: dockerBuildContent)
 
                 dockerOperations.dockerPush(ecrRepoURL: ecrRepoURL,
                  region: region,
@@ -56,8 +63,8 @@ podTemplate(yaml: '''
                  customImage: customImage)
             }
             stage("Slack notification") {
-                slackMessage = env.JOB_NAME + """ with Branch=""" + env.BRANCH_NAME + """ finished successfully:
-        Image URI: 043196765225.dkr.ecr.us-east-1.amazonaws.com/jenkins-test:test
+                slackMessage = env.JOB_NAME + """Job with Branch=""" + env.BRANCH_NAME + """ finished successfully:
+        Image URI: """ + ecrRepoURL + """/""" + repoName + """:""" + imageTag + """
         Jenkins BUILD_URL: """ + env.BUILD_URL + """
         Jenkins BUILD_LOG: """ + env.BUILD_URL + """console
         """
