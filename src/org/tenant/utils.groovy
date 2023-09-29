ceb9d64efffceb3acf5640eb4a7a3c26e876d2b8
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-29 18:32:09 +0530

    test jenkins

--- src/org/tenant/utils.groovy
+++ src/org/tenant/utils.groovy
@@ -1,6 +1,6 @@
 package org.tenant
 
-class utils implements Serializable {
+class utils {
     def scmCheckoutToBranch(scmUrl, credentialsId, branchName) {
         checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
     }
