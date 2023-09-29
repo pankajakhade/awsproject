5bc1541835899c6e313a503a4199f2339acf10f4
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-29 18:39:02 +0530

    test jenkins

--- src/org/tenant/jenkinsCI.groovy
+++ src/org/tenant/jenkinsCI.groovy
@@ -2,6 +2,7 @@ package org.tenant
 
 class jenkinsCI {
     def scmCheckoutToBranch(scmUrl, credentialsId, branchName) {
-        checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
+        println("Hello World")
+        //checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
     }
 }
