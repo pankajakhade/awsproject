0743a45dd10d38a6bf4e1ae79188f3cb5593d9b3
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-29 18:20:29 +0530

    test jenkins

--- src/org/tenant/utils.groovy
+++ src/org/tenant/utils.groovy
@@ -2,7 +2,9 @@ package org.tenant
 
 class utils implements Serializable {
     def scmCheckoutToBranch(Map params) {
-        println("params" + params)
-        //checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
+        def scmUrl = params.scmUrl ?: error("scmUrl is required.")
+        def branchName = params.branchName ?: error("scmUrl is required.")
+        def credentialsId = params.credentialsId ?: error("credentialsId is required.")
+        checkout scmGit(branches: [[name: "*/$branchName"]], extensions: [], userRemoteConfigs: [[credentialsId: credentialsId, url: scmUrl]])
     }
 }
