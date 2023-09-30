8aaff2b81a296108f6dbec8639fb042e89530e4f
Pankaj Akhade <pankaj.akhade@opcito.com>
2023-09-30 13:40:41 +0530

    test jenkins

--- vars/slackSendMessages.groovy
+++ vars/slackSendMessages.groovy
@@ -7,7 +7,7 @@ Jenkins BUILD_LOG: """ + env.BUILD_URL + """console
     slackSend channel: channelName, color: 'good', failOnError: true , message: slackMessage, teamDomain: 'storelocal', tokenCredentialId: 'slack-token'
 }
 
-def slackSendFailureMessage() {
+def slackSendFailureMessage(channelName) {
     slackMessage = """**""" + env.JOB_NAME + """ Job with Branch=""" + env.BRANCH_NAME + """ is failed:**
 Jenkins BUILD_URL: """ + env.BUILD_URL + """
 Jenkins BUILD_LOG: """ + env.BUILD_URL + """console
